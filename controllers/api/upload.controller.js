d9c5c960d3ff549b7101c873fdf855257c35dbb0
isakgranqvist2021 <isakwebdev@gmail.com>
2021-08-09 23:13:13 +0200

    qwt

--- controllers/api/upload.controller.js
+++ controllers/api/upload.controller.js
@@ -3,15 +3,23 @@ import fileModel from '../../models/file.model';
 import orderModel from '../../models/order.model';
 
 async function upload(req, res) {
-    const files = await Promise.all(req.files.map(async (file) => {
-        return await cloud.upload(file.filename);
-    }));
+    try {
+        const files = await Promise.all(req.files.map(async (file) => {
+            return await cloud.upload(file.filename);
+        }));
 
-    return res.json({
-        message: `${req.files.length} files uploaded`,
-        success: true,
-        data: files
-    });
+        return res.json({
+            message: `${req.files.length} files uploaded`,
+            success: true,
+            data: files
+        });
+    } catch(err) {
+        return res.json({
+            message: `upload failed`,
+            success: false,
+            data: null
+        });
+    }
 }
 
 
