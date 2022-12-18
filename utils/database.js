fbefcb819362a70521857fa0af9c9485c6419ce1
isakgranqvist2021 <isakwebdev@gmail.com>
2022-12-18 21:09:12 +0100

    Minor fixes

--- utils/database.js
+++ utils/database.js
@@ -1,29 +1,27 @@
-import mongoose from 'mongoose';
-import env from './env';
+import mongoose from "mongoose";
+import env from "./env";
 
 async function connect() {
-    try {
-        await mongoose.connect(env.DB_URI, {
-            useCreateIndex: true,
-            useFindAndModify: true,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+  try {
+    await mongoose.connect(env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log('MongoDB has connected');
-    } catch (err) {
-        console.log(err);
-    }
+    console.log("MongoDB has connected");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function disconnect() {
-    try {
-        await mongoose.disconnect();
+  try {
+    await mongoose.disconnect();
 
-        console.log('MongoDB has disconnected');
-    } catch (err) {
-        console.log(err);
-    }
+    console.log("MongoDB has disconnected");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export default { connect, disconnect };
--- utils/texts.js
+++ utils/texts.js
@@ -1,18 +1,18 @@
-import env from './env';
-import twilio from 'twilio';
-
-const client = twilio(env.TWILIO_ACCOUNT_SID, env.TWILIO_AUTH_TOKEN);
+import env from "./env";
+import twilio from "twilio";
 
 async function sendText(message, to) {
-    try {
-        return await client.messages.create({
-            body: message,
-            from: env.TWILIO_PHONE_NUMBER,
-            to: to
-        });
-    } catch (err) {
-        return Promise.reject(err);
-    }
+  const client = twilio(env.TWILIO_ACCOUNT_SID, env.TWILIO_AUTH_TOKEN);
+
+  try {
+    return await client.messages.create({
+      body: message,
+      from: env.TWILIO_PHONE_NUMBER,
+      to: to,
+    });
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 export default { sendText };
